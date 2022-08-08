0f58c7f611ecace2c0d4c1bc74ad8b1fa6fd14bd
Jesus Ale <jesus.9314@gmail.com>
2022-08-07 23:45:06 -0500

    diseño de carrito responsive

--- js/main.js
+++ js/main.js
@@ -5,23 +5,36 @@ const btnCart = document.querySelector("#btn-cart")
 const navbarItems = document.querySelector("#list")
 const cart = document.querySelector("#cart")
 const closeCart = document.querySelector("#close")
+const fullCart = document.querySelector("#fullCart")
 
 btnToggle.addEventListener('click', () => {
-    if(cart.className === "cart appear"){
-        cart.classList.remove("appear")
-        navbarItems.classList.add("appear")
+    if(cart.className === "cart appearCart"){
+        cart.classList.remove("appearCart")
+        navbarItems.classList.add("appearMenu")
     }else{
-        navbarItems.classList.toggle("appear")
+        navbarItems.classList.toggle("appearMenu")
+    }
+    if(fullCart.className === "fullCart show"){
+        fullCart.classList.remove("show")
     }
 })
 btnCart.addEventListener('click',()=>{
-    if(navbarItems.className=== "navbar__items appear"){
-        navbarItems.classList.remove("appear")
-        cart.classList.add("appear")
+    fullCart.classList.toggle("show")
+    if(navbarItems.className=== "navbar__items appearMenu"){
+        navbarItems.classList.remove("appearMenu")
+        cart.classList.add("appearCart")
+        
     }else{
-        cart.classList.toggle("appear")
+        cart.classList.toggle("appearCart")
     }
 })
 closeCart.addEventListener('click',()=>{
-    cart.classList.remove("appear")
+    cart.classList.remove("appearCart")
+    fullCart.classList.remove("show")
+})
+fullCart.addEventListener('click',()=>{
+    if(fullCart.className === "fullCart show"){
+        cart.classList.remove("appearCart")
+        fullCart.classList.remove("show")
+    }
 })
