52d08e4ee42f809d3acd95de73e946a2531c3da5
Jesus Ale <jesus.9314@gmail.com>
2022-08-10 16:25:50 -0500

    Mejora de menú y carrito

--- js/main.js
+++ js/main.js
@@ -8,36 +8,54 @@ const closeCart = document.querySelector("#close")
 const fullCart = document.querySelector("#fullCart")
 
 btnToggle.addEventListener('click', () => {
-    if(cart.className === "cart appearCart"){
+    if (cart.className === "cart appearCart") {
         cart.classList.remove("appearCart")
         navbarItems.classList.add("appearMenu")
-    }else{
+    } else {
         navbarItems.classList.toggle("appearMenu")
     }
-    if(fullCart.className === "fullCart show"){
+    if (fullCart.className === "fullCart show") {
         fullCart.classList.remove("show")
     }
 })
-btnCart.addEventListener('click',()=>{
+btnCart.addEventListener('click', () => {
     fullCart.classList.toggle("show")
-    if(navbarItems.className=== "navbar__items appearMenu"){
+    if (navbarItems.className === "navbar__items appearMenu") {
         navbarItems.classList.remove("appearMenu")
         cart.classList.add("appearCart")
-        
-    }else{
+
+    } else {
         cart.classList.toggle("appearCart")
     }
 })
-closeCart.addEventListener('click',()=>{
+closeCart.addEventListener('click', () => {
     cart.classList.remove("appearCart")
     fullCart.classList.remove("show")
 })
-fullCart.addEventListener('click',(e)=>{
-    let sectionClicked = window.innerWidth - cart.clientWidth
-    if(e.clientX <= sectionClicked){
-        if(fullCart.className === "fullCart show"){
-            fullCart.classList.remove("show")
-            cart.classList.remove("appearCart")
-        }
+window.addEventListener('click', e => {
+    if (e.target === fullCart) {
+        fullCart.classList.remove("show")
+        cart.classList.remove("appearCart")
     }
 })
+
+//slick
+$(document).ready(function(){
+    $('.products').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        resposive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+            }
+        ]
+    });
+  });
---
+++ readme.md
@@ -0,0 +1,31 @@
+# QuantumWorld
+
+Proyecto final del curso de JavaScript en [CoderHouse](coderhouse.com), empleando las siguientes tecnologías
+
+* HTML5 
+* Css3
+* JavaScript
+* Sass
+
+## Fuentes ✒️
+
+* [League Spartan - Google Fonts](https://fonts.google.com/specimen/League+Spartan?query=sparta)
+* [Poppins - Google Fonts](https://fonts.google.com/specimen/Poppins?query=poppins)
+* [Roboto - Google Fonts](https://fonts.google.com/specimen/Roboto?query=roboto)
+
+## Colores 🌈
+
+* Very Dark Purple: #140e19
+* Dark Purple: #1d2a49
+* Purple: #484982
+* Light Purple: #856cc4
+* Very Dark Purple: #d896fa
+
+## Breakpoints 📃
+
+* 768px
+* 992px
+* 1200px
+
+
+
